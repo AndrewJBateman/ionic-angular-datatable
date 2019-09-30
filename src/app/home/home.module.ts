80d0692f863ed07e3fea88865e1ff64d1ba8732e
Andrew Bateman <gomezbateman@yahoo.com>
2019-09-30 13:57:02 +0200

    tutorial code complete and working

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -16,7 +18,8 @@ import { HomePage } from './home.page';
         path: '',
         component: HomePage
       }
-    ])
+    ]),
+    NgxDatatableModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 
+import data from '../../assets/company.json';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public companies = data;
+  tableStyle = 'bootstrap';
+  customRowClass = false;
 
-  constructor() {}
+  constructor() {
+    console.log(this.companies);
+  }
+
+  switchStyle() {
+    if (this.tableStyle === 'dark') {
+      this.tableStyle = 'bootstrap';
+    } else {
+      this.tableStyle = 'dark';
+    }
+  }
+
+  getRowClass(row) {
+    console.log('class: ', row);
+    const isMale = row.gender === 'male';
+
+    if (!this.customRowClass) {
+      return {};
+   }
+    return {
+      'male-row': isMale,
+      'female-row': !isMale
+    };
+  }
+
+  async open(row) {
+    console.log(row);
+  }
 
 }
